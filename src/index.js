bae8587e872b75d845e461ea54ce7469d77c3eb8
itsca <itscae@gmail.com>
2017-07-05 15:04:25 -0600

    Created first Component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDom from 'react-dom';
+import App from './app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDom.render(
+	<App />, 
+	document.getElementById('root')
+);
